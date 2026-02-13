18d30126dd4e8d6c0b5992da06ddde6c0ac9350c
mehtalucky492 <110759276+mehtalucky492@users.noreply.github.com>
2026-02-13 15:30:30 +0530

    Fix condition for button class assignment

--- scripts/aem.js
+++ scripts/aem.js
@@ -422,7 +422,8 @@ function decorateButtons(element) {
     if (a.href !== a.textContent) {
       const up = a.parentElement;
       const twoup = a.parentElement.parentElement;
-      if (!a.querySelector('img')) {
+      const thirdup = twoup.parentElement;
+      if (!a.querySelector('img') && !twoup.tagName === "LI") {
         if (up.childNodes.length === 1 && (up.tagName === 'P' || up.tagName === 'DIV')) {
           a.className = 'button'; // default
           up.classList.add('button-container');
